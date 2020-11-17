b882522906f65c643246df7544b166845bb198b9
Eduardo Pessanha <poppessanha@gmail.com>
2020-11-17 20:20:37 -0300

    Atualizações

---
+++ meusprojetos/emoji.md
@@ -0,0 +1,2 @@
+Endereço de repositório de emoji da Ika - Ikatyang
+https://github.com/ikatyang/emoji-cheat-sheet
---
+++ meusprojetos/projetosPython/pacote/numeros.py
@@ -0,0 +1,5 @@
+from uteis import fatorial
+
+num = int(input('Digite um número: '))
+fat = fatorial(num)
+print(f'O fatorial de {num} é {fat}')
